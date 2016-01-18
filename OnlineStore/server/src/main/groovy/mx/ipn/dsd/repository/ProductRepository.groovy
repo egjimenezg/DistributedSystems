b8adff528bcc4fdd8c7a6080d02578bb2d54899f
egjimenezg <egjimenezg@gmail.com>
2016-01-18 05:41:13 -0600

    coding #3 adding product repository query

--- OnlineStore/server/src/main/groovy/mx/ipn/dsd/repository/ProductRepository.groovy
+++ OnlineStore/server/src/main/groovy/mx/ipn/dsd/repository/ProductRepository.groovy
@@ -1,11 +1,13 @@
-package mx.ipn.dsd.config.repository
+package mx.ipn.dsd.repository
 
 import org.springframework.data.domain.Sort
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.jpa.repository.Query
 import mx.ipn.dsd.domain.Product
 
 interface ProductRepository extends CrudRepository<Product, Long>{
 
+  @Query("FROM Product p")
   List<Product> findAll()
 
 }
