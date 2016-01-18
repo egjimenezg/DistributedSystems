9f709c0ef8a1cfcbec812da12f1532b67188752a
egjimenezg <egjimenezg@gmail.com>
2016-01-18 05:45:27 -0600

    coding #3 fixing product repository service

---
+++ OnlineStore/server/src/main/groovy/mx/ipn/dsd/services/impl/ProductServiceImpl.groovy
@@ -0,0 +1,22 @@
+package mx.ipn.dsd.services.impl
+
+import java.rmi.Remote
+import java.rmi.RemoteException
+import mx.ipn.dsd.services.ProductService
+import mx.ipn.dsd.model.ProductInterface
+import mx.ipn.dsd.domain.Product
+import mx.ipn.dsd.repository.ProductRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import javax.sql.DataSource
+
+
+class ProductServiceImpl implements ProductService{
+
+  @Autowired
+  ProductRepository productRepository
+
+  List<ProductInterface> getProducts() throws RemoteException{
+    productRepository.findAll()
+  }
+}
