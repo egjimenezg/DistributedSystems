cf38c20bcbfb2d78e6f951326a6d9352aeb88a68
egjimenezg <egjimenezg@gmail.com>
2016-01-18 19:10:10 -0600

    coding #5 inyectando correctamente los beans al context de Spring

--- OnlineStore/server/src/main/groovy/mx/ipn/dsd/Server.groovy
+++ OnlineStore/server/src/main/groovy/mx/ipn/dsd/Server.groovy
@@ -8,37 +8,44 @@ import mx.ipn.dsd.services.ProductService
 import mx.ipn.dsd.services.impl.ProductServiceImpl
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.data.jpa.repository.JpaContext
 import mx.ipn.dsd.config.ApplicationConfig
 import mx.ipn.dsd.config.DataSourceConfig
 import mx.ipn.dsd.config.ServiceConfig
 import javax.sql.DataSource
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory
 
+class Server {
 
-class Server implements Runnable{
+  @Autowired
+  ProductService productService
 
-  @Override
-  public void run(){
-    if(System.getSecurityManager() == null){
-      System.setSecurityManager(new SecurityManager())
-    }
-    try{
-      final Registry registry = LocateRegistry.createRegistry(Registry.REGISTRY_PORT)
-      ProductService productService = new ProductServiceImpl()
-      ProductService stub = (ProductService) UnicastRemoteObject.exportObject(productService,0)
-      registry.rebind("ProductService",stub) 
-    }
-    catch(RemoteException e){
-      e.printStackTrace()
-    }
-  }
-
-  static main(def args){
+  public Server() {
     ApplicationContext ctx = new AnnotationConfigApplicationContext()
     ctx.register(ApplicationConfig.class)
     ctx.register(DataSourceConfig.class)
     ctx.register(ServiceConfig.class)
     ctx.refresh()
-    Thread thread = new Thread(new Server())  
-    thread.start()
+
+    AutowireCapableBeanFactory server = ctx.getAutowireCapableBeanFactory()
+    server.autowireBean(this)
+  }
+
+  static main(def args){
+    Server server = new Server();
+
+    if(System.getSecurityManager() == null){
+      System.setSecurityManager(new SecurityManager())
+    }
+    try{
+      final Registry registry = LocateRegistry.createRegistry(Registry.REGISTRY_PORT)
+      ProductService stub = (ProductService) UnicastRemoteObject.exportObject(server.productService, 0)
+      registry.rebind("ProductService",stub)
+    }
+    catch(RemoteException e){
+      e.printStackTrace()
+    }
   }
 }
--- OnlineStore/server/src/main/groovy/mx/ipn/dsd/config/ServiceConfig.groovy
+++ OnlineStore/server/src/main/groovy/mx/ipn/dsd/config/ServiceConfig.groovy
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.ComponentScan
 
 @Configuration
-@ComponentScan(basePackages=["mx.ipn.dsd.services"])
+@ComponentScan(basePackages=["mx.ipn.dsd"])
 class ServiceConfig{
 }
